734ca671c1b2d4e9bb0fd5ae206ab9d30206946a
Saša Jurić <sasa.juric@gmail.com>
2020-01-26 10:17:29 +0100

    Update formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
   inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [check: 1, gen: 1]
+  locals_without_parens: [check: :*, all: :*, gen: :*]
 ]
