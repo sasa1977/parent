6fc37beefc4d8b0e64900ccc0ffd0e986fd885b9
Saša Jurić <sasa.juric@gmail.com>
2020-09-09 20:19:22 +0200

    Use Parent.Supervisor

--- lib/parent/application.ex
+++ lib/parent/application.ex
@@ -4,9 +4,8 @@ defmodule Parent.Application do
 
   @impl Application
   def start(_type, _args) do
-    Supervisor.start_link(
+    Parent.Supervisor.start_link(
       [Parent.MetaRegistry],
-      strategy: :one_for_one,
       name: __MODULE__
     )
   end
