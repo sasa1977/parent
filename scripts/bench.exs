ba1933fc2ccb66c2835d468e25de127aaf43237f
Saša Jurić <sasa.juric@gmail.com>
2020-09-10 09:20:38 +0200

    Add a basic bench script

---
+++ scripts/bench.exs
@@ -0,0 +1,39 @@
+{:ok, parent} = Parent.Supervisor.start_link([])
+
+:timer.tc(fn ->
+  Enum.each(
+    1..100_000,
+    fn _i ->
+      Parent.Client.start_child(
+        parent,
+        %{start: {Agent, :start_link, [fn -> :ok end]}, restart: :temporary}
+      )
+    end
+  )
+end)
+|> elem(0)
+|> Kernel.div(1000)
+|> IO.inspect()
+
+IO.inspect(Process.info(parent, :reductions))
+IO.inspect(Process.info(parent, :memory))
+
+{:ok, sup} = DynamicSupervisor.start_link(strategy: :one_for_one)
+
+:timer.tc(fn ->
+  Enum.each(
+    1..100_000,
+    fn i ->
+      DynamicSupervisor.start_child(
+        sup,
+        %{id: i, start: {Agent, :start_link, [fn -> :ok end]}, restart: :temporary}
+      )
+    end
+  )
+end)
+|> elem(0)
+|> Kernel.div(1000)
+|> IO.inspect()
+
+IO.inspect(Process.info(sup, :reductions))
+IO.inspect(Process.info(sup, :memory))
